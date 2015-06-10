5377ec53609fbad24b1776642cfb0db1e4f64e57
tvancann <tvancann@thunderhead.com>
2015-06-10 19:41:07 +0200

    Added tfidf test

--- tests/feature_extraction/test_TFIDF.py
+++ tests/feature_extraction/test_TFIDF.py
@@ -1,11 +1,25 @@
+from FeatureManager import FeatureManager
+from feature_extraction.TFIDF import TFIDF
+
 __author__ = 'tvancann'
 
 import unittest
 
 
 class TestTFIDF(unittest.TestCase):
-	def test_something(self):
-		self.assertEqual(True, True)
+	def test_add_to_voc(self):
+		tfidf = TFIDF(FeatureManager())
+		res = tfidf.single_doc_tfidf([])
+		self.assertEqual(res.shape, (1, 0))
+
+	def test_update_tfidf(self):
+		tfidf = TFIDF(FeatureManager())
+		tfidf.update_tfidf(["hello", "my", "name", "is", "Tim"])
+		tfidf.update_tfidf(["hello", "my", "cat", "is", "called", "Isis"])
+		self.assertEqual(tfidf.feature_manager.feature_dimensions()[1], 8)
+		self.assertEqual(tfidf.feature_manager.feature_dimensions()[0], 2)
+
+		res = tfidf.get_tfidf()
 
 
 if __name__ == '__main__':
