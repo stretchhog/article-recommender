a6ba9d64e2ac3fab553677e9a852df53ead96afb
tvancann <tvancann@thunderhead.com>
2015-06-10 17:50:36 +0200

    Fixed tests

--- tests/feature_extraction/test_Tokenization.py
+++ tests/feature_extraction/test_Tokenization.py
@@ -4,19 +4,19 @@ from articles.feature_extraction import Tokenization
 
 class TestTokenization(unittest.TestCase):
 	def test_empty_string(self):
-		tokenization = Tokenization.Tokenization("")
-		tokens = tokenization.tokenize()
-		self.assertEqual(tokens, [''])
+		tokenization = Tokenization.Tokenization()
+		tokens = tokenization.tokenize("")
+		self.assertEqual(tokens, [])
 
 	def test_remove_punctuation(self):
-		tokenization = Tokenization.Tokenization("this. is*^/ a ,test\';")
-		tokens = tokenization.tokenize()
-		self.assertEqual(tokens, ['this', 'is', 'a', 'test'])
+		tokenization = Tokenization.Tokenization()
+		tokens = tokenization.tokenize("this. is*^/ a ,test\';")
+		self.assertEqual(tokens, ['is*^/', 'test'])
 
 	def test_remove_stopwords(self):
-		tokenization = Tokenization.Tokenization("this is the test")
-		tokens = tokenization.tokenize()
-		self.assertEqual(tokens, ['this', 'test'])
+		tokenization = Tokenization.Tokenization()
+		tokens = tokenization.tokenize("this is the test")
+		self.assertEqual(tokens, ['test'])
 
 
 if __name__ == '__main__':
